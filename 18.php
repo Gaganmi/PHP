6aea8fbd2490a567e65bd39fa662f0e73300eaa7
Gaganmi <101922832+Gaganmi@users.noreply.github.com>
2022-06-14 23:41:18 +0530

    Add files via upload

---
+++ 18.php
@@ -0,0 +1,10 @@
+<?php
+print_r("GAGANDEEP SINGH<br>");   
+echo"<br>";
+echo readfile("file.txt");   
+$a=1;
+echo"<br>";
+var_dump($a);  
+echo"<br>";
+echo strlen('Hello I am gagandeep. Welcome to my program');   
+?>
